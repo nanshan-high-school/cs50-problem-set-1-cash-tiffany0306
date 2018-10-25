5d6e643a8279cd813093cfddd39b887718f250b6
tiffany0306 <43640422+tiffany0306@users.noreply.github.com>
2018-10-25 19:28:21 +0800

    Create 25104陳宥筑.cpp

---
+++ 25104陳宥筑.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+int a=50,b=10,c=5,d=1;
+int money;
+cout << "請輸入你的錢";
+cin >> money;
+
+int e = money / a, f = money - e * a; 
+cout << "\n"<<"有" << e << "個50 ";
+int g=f/b,h=f-g*b;
+cout <<"\n" << g << "個10";
+int i=h/c,j=h-i*c;
+cout<<"\n"<<i<<"個5";
+int k=j/d;
+cout<<"\n"<<"跟"<<k<<"個1"; 
+
+
+
+
+}
