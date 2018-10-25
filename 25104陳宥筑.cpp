d062ab022fb5632b2d58d39be30bb6277d317531
tiffany0306 <43640422+tiffany0306@users.noreply.github.com>
2018-10-25 19:36:05 +0800

    Update 25104陳宥筑.cpp

--- 25104陳宥筑.cpp
+++ 25104陳宥筑.cpp
@@ -6,7 +6,6 @@ int a=50,b=10,c=5,d=1;
 int money;
 cout << "請輸入你的錢";
 cin >> money;
-
 int e = money / a, f = money - e * a; 
 cout << "\n"<<"有" << e << "個50 ";
 int g=f/b,h=f-g*b;
@@ -15,8 +14,4 @@ int i=h/c,j=h-i*c;
 cout<<"\n"<<i<<"個5";
 int k=j/d;
 cout<<"\n"<<"跟"<<k<<"個1"; 
-
-
-
-
 }
